04e65232b3b6861dc9f6da15694a93fe7341bcc0
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-19 08:32:15 +0300

    created three constructors

---
+++ index.js
@@ -0,0 +1,21 @@
+//created a constructor function scooter
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+//created driver constructor
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+//created PickupLocatio constructor
+
+function PickupLocation(address, city) {
+    this.address = address;
+    this.city = city;
+  }
+  
